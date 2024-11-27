2a9118530c8c29b00b79091ad764d3b5b12bdd3e
Denilza <122271712+denilzalopes@users.noreply.github.com>
2024-11-27 15:18:47 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    
+      <div className='container'>
+        <h1>Menu Digital</h1>
+        <div className='card-grid'>
+          
+        </div>
+    
+      </div>
+     
+  
+  )
+}
+
+export default App
